eecdf388cb655cb8b1b0df327945ba834160acbe
Budur Alshammari <121249987+budur1@users.noreply.github.com>
2023-12-15 23:08:36 +0300

    Update password_vali.php

--- password_vali.php
+++ password_vali.php
@@ -1,3 +1,5 @@
+//Basic PHP script that prompts the user to enter a password and checks if the provided password matches a predefined value.
+
 <?php
 echo"What is the password?\n";
 $pass = readline();
