53c8ce747331213590618adf0328fa05c8386384
Budur Alshammari <121249987+budur1@users.noreply.github.com>
2023-12-15 23:12:05 +0300

    Add files via upload

---
+++ adding_nums.php
@@ -0,0 +1,15 @@
+<?php 
+echo "Enter a number:\n";
+$fnum = readline();
+echo "Enter a number:\n";
+$snum = readline();
+echo "Enter a number:\n";
+$tnum = readline();
+echo "Enter a number:\n";
+$fonum = readline();
+echo "Enter a number:\n";
+$finum = readline();
+$total = $fnum + $snum + $tnum + $fonum + $finum;
+echo "The total is $total.";
+
+?>
